ceaba220faef690f97ad57ae6c7d0f8f756a48da
Marcin Weglowski <marcin@escali.no>
2019-03-05 07:29:23 +0100

    RED: For Empty input string, Add should return 0.

---
+++ StringBuilderTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringBuilderTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringBuilderTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("73fd1ff2-0fe8-4c31-9d00-ee9cad48565c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StringBuilderTests/StringBuilderTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringBuilderTests
+{
+    [TestClass]
+    public class StringBuilderTests
+    {
+        [TestMethod]
+        public void shouldReturnZeroForEmptyInputString()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+            int actual = stringCalculator.Add("");
+
+            Assert.AreEqual(0, actual);
+        }
+    }
+}
