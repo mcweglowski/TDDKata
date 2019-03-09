7f817216e7946c18166ab02ceac5b7e5debcce4b
Marcin Weglowski <marcin@escali.no>
2019-03-09 17:17:54 +0100

    GREEN: Allow optional delimiter in input string

--- TDDKata/StringCalculator.cs
+++ TDDKata/StringCalculator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,11 @@ namespace TDDKata
 {
     public class StringCalculator
     {
+        private char[] DefaultDelimiters = new char[] { ',', '\n' };
+
         public int Add(string addends)
         {
-            if (string.Empty == addends)
+            if (String.Empty == addends)
             {
                 return 0;
             }
@@ -20,9 +23,20 @@ namespace TDDKata
 
         private int Sum(string addends)
         {
+            if (1 < addends.Length)
+            {
+                if ("//" == addends.Substring(0, 2))
+                {
+                    StringReader stringReader = new StringReader(addends);
+                    string header = stringReader.ReadLine();
+                    DefaultDelimiters = header.Substring(2, 1).ToCharArray();
+                    addends = stringReader.ReadLine();
+                }
+            }
+
             int sum = 0;
 
-            foreach (string addend in addends.Split(new char[] { ',' , '\n'} ))
+            foreach (string addend in addends.Split(DefaultDelimiters))
             {
                 sum = sum + int.Parse(addend);
             }
